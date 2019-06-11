c4ceb67627e053e25347c4485b1dec54a5336428
Jeremy Ginnard <jeremyginnard@gmail.com>
2019-06-11 19:25:31 -0400

    Updated demo output text for readability.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const DATA_DIR = './data/';
 fs.readdir(DATA_DIR, function(error, files){
     files.forEach(function(file){
         let wordSearch = new WordSearch(DATA_DIR + file);
-        console.log('-------- Word Locations for ' + file + ' --------');
+        console.log('\n-------- Word Locations for ' + file + ' --------\n');
         console.log(wordSearch.getAllWordLocations());
     });
 });
