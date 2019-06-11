90f9967c5c9294ee61a8bd4888eb99888e72a071
Jeremy Ginnard <jeremyginnard@gmail.com>
2019-06-11 14:43:37 -0400

    Added demo and updated README.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# kata-word-search
-Word Search Kata
+# Word Search Kata
+
+## Setting up the project
+1. Before you start, ensure that you have [nodeJS](https://nodejs.org) and [npm](https://www.npmjs.com/get-npm) installed.
+2. Install dependencies
+```
+npm install
+```
+3. Run tests
+```
+npm test
+```
+4. Run demo
+```
+npm start
+```
+
---
+++ index.js
@@ -0,0 +1,11 @@
+const WordSearch = require('./models/WordSearch');
+const fs = require('fs');
+const DATA_DIR = './data/';
+
+fs.readdir(DATA_DIR, function(error, files){
+    files.forEach(function(file){
+        let wordSearch = new WordSearch(DATA_DIR + file);
+        console.log('-------- Word Locations for ' + file + ' --------');
+        console.log(wordSearch.getAllWordLocations());
+    })
+});
