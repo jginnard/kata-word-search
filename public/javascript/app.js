be46c680d2d0068f4a375672891cc6e4341dae00
Jeremy Ginnard <jeremyginnard@gmail.com>
2019-06-12 22:29:16 -0400

    Display grid with highlighted words.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -9,8 +9,14 @@ function solveWordSearch() {
         readFile(inputFiles[0]).then(function(content) {
             wordSearch.readString(content);
             $('#wordPositions').text(wordSearch.getAllWordLocations());
-        }).catch(function() {
-            window.alert('Error reading file. Please check the file and try again.');
+            let grid = wordSearch.getDisplayGrid().map(function(row) {
+                return row.join('');
+            }).join('\n');
+            $('#grid').text(grid);
+            highlightWords(wordSearch);
+        }).catch(function(e) {
+            window.alert('Error. Please check the file and try again.');
+            throw e;
         });
     }
 }
@@ -31,6 +37,60 @@ function readFile(file) {
         };
         reader.readAsText(file, "UTF-8");
     });
+}
+
+/**
+ * Highlights word locaitons on the displayed grid
+ * @param {Object} wordLocations 
+ */
+function highlightWords(wordSearch) {
+    let wordLocations = wordSearch.getWordLocationData();
+    let gridSize = wordSearch.getDisplayGrid().length;
+    for (let word in wordLocations) {
+        let location = wordLocations[word];
+        $('#grid').append(getHighlightingElement(gridSize, location, word));
+    }
+}
+
+const TRANSFORMS = {
+    "-1":{
+        "-1": "transform: rotate(135deg) translate(-18px,-6px);",
+        "0": "transform: rotate(180deg) translate(-18px,-4px);",
+        "1": "transform: rotate(225deg) translate(-22px,0px);"
+    },
+    "0":{
+        "-1": "transform: rotate(90deg) translate(-6px,-4px);",
+        "1": "transform: rotate(270deg) translate(-18px,4px);"
+    },
+    "1": {
+        "-1": "transform: rotate(45deg) translate(-10px,0px);",
+        "0": "transform: rotate(0deg) translate(-8px,4px);",
+        "1": "transform: rotate(315deg) translate(-18px,6px);"
+    }
+};
+
+/**
+ * Get the element to highlight a given word
+ * @param {number} gridSize - size of the grid, equal to height or width. 
+ * @param {Object} location - location object of the word
+ * @param {String} word
+ */
+function getHighlightingElement(gridSize, location, word) {
+    let scale = 'calc((100% / ' + gridSize + ') * ';
+    let top = 'top:' + scale + location[0].y + ');';
+    let left = 'left:' + scale + location[0].x + ');';
+    let dx = location[1].x - location[0].x;
+    let dy = location[0].y - location[1].y;
+    let length = (dx == 0 || dy == 0)? location.length : location.length * Math.SQRT2;
+    let width = 'width:' + scale + length + ');';
+    let transform = TRANSFORMS[dx][dy];
+    let style = top + left + width + transform;
+    let element = '<span id="' + word +'" class="highlight" style="' + style + '"></span>';
+    return element;
+}
+
+function getWordPositionElement(word, wordPosition) {
+    return '<div class="word-position" data-word="' + word + '">';
 }
 
 /**
--- testweb/testweb.js
+++ testweb/testweb.js
@@ -7,8 +7,8 @@ function runMochaTests() {
             it('There is a form with id wordSearchForm', function() {
                 assert.equal($('form#wordSearchForm').length, 1);
             });
-            it('There is a input of type file', function() {
-                assert.equal($('input[type=file]').length, 1);
+            it('There is a input of type file with id inputFile', function() {
+                assert.equal($('input[type=file]#inputFile').length, 1);
             });
             it('There is a button with id solveWordSearch', function () {
                 assert.equal($('button#solveWordSearch').length, 1);
@@ -17,9 +17,15 @@ function runMochaTests() {
                 assert.equal($('button#solveWordSearch').text(), 'Solve');
             });
         });
+        describe('Elements for displaying results', function() {
+            it('There is a div with id grid', function() {
+                assert.equal($('div#grid').length, 1);
+            });
+            it('There is a div with id wordPositions', function() {
+                assert.equal($('div#wordPositions').length, 1);
+            });
+        });
     });
-
-
     window.mocha.run();
 }
 
